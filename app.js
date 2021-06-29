87c8f7305a07c7281afcc3025e19d64523eeae10
David Castro <63515575+devcastro@users.noreply.github.com>
2021-06-29 08:33:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const opts = {
     },
     identity:{
         username:"acastrobot",
-        password:"oauth:8netmcwxgk1a798q7alt9oo4by2ohk",
+        password:" HIDDEN FOR SECURITY AND PRIVACY REASONS ",
     },
     channels: [
         "actioncastro",
